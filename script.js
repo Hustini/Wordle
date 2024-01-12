60debf9eb3021fff8a610ae8144ac7313bc0b348
ejders-bzz <ejders@bzz.ch>
2024-01-12 13:16:16 +0100

    winning is now possible

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let button = document.getElementById("button");
-    let word = document.getElementById("word");
-    const url = "https://random-word-api.vercel.app/api?words=1&length=4"
+    let show = document.getElementById("show");
+    let solution = document.getElementById("solution");
+    let word;
+    const url = "https://random-word-api.vercel.app/api?words=1&length=4";
+    let input = document.getElementById("input");
+    let submit = document.getElementById("submit");
+    let item = document.getElementsByClassName("grid-item");
 
-    button.addEventListener("click", function () {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => word.innerText = data)
-            .catch(error => console.error('Error:', error));
-    })
+    fetch(url)
+        .then(response => response.json())
+        .then(data => word = data)
+        .catch(error => console.error('Error:', error));
+
+    show.addEventListener("click", function () {
+        solution.innerText = word
+    });
+
+    submit.addEventListener("click", function () {
+        if (input.value.length !== 4) {
+            alert("It is a word with 4 letters")
+            input.value = ""
+        }
+        if (word == input.value) {
+            let split_word = input.value.split('');
+            for (let i = 0; i < split_word.length; i++) {
+                item[i].innerText = split_word[i]
+            }
+        }
+    });
 });
